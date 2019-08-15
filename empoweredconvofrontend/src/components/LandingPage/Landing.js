f119339e61f56ed98cce1d57dc6f96a5743a08e2
Nolan Allen <nolan.r.allen@gmail.com>
2019-08-14 19:36:35 -0500

    Latter-stage additions (landing page statement) positioned with responsive figures, must convert rest of landing page

--- empoweredconvofrontend/src/components/LandingPage/Landing.js
+++ empoweredconvofrontend/src/components/LandingPage/Landing.js
@@ -31,25 +31,51 @@ export default class Landing extends React.Component {
           <div className="landing-stage-trap" />
         </div> */}
         <div className="landing-float-plane">
-          <div className="landing-vid-floater">Video goes here.</div>
-          <div className="landing-text-floater">
-            <h3>One Conversation at a Time</h3>
-            <p>
-              <span>
-                Our tool takes the burden off of the survivor for having to
-              </span>
-              <span style={{ fontWeight: 'bold' }}>
-                {' '}
-                educate their loved ones about assault{' '}
-              </span>
-              <span>
-                during disclosure because we provide that training ahead of
-                time.
-              </span>
-            </p>
-            <Link to="/form">
-              <button>Use The Tool</button>
-            </Link>
+          <div className="landing-float-centerpiece">
+            <div className="landing-vid-floater">Video goes here.</div>
+            <div className="landing-text-floater">
+              <h3>One Conversation<br/>at a Time</h3>
+              <p>
+                <span>
+                  Our tool takes the burden off of the survivor for having to
+                </span>
+                <span style={{ fontWeight: 'bold' }}>
+                  {' '}
+                  educate their loved ones about assault{' '}
+                </span>
+                <span>
+                  during disclosure because we provide that training ahead of
+                  time.
+                </span>
+              </p>
+              <Link to="/form">
+                <button>Use The Tool</button>
+              </Link>
+            </div>
+          </div>
+          <div className="landing-statement">
+            <h2>What We Do</h2>
+            <div className="bullet-point">
+              <div className="bullet">1</div>
+              <div className="bullet-point-text">
+                <h4>Communicate</h4>
+                <p>We've got your back when it comes to anonymously reaching out to a loved one!</p>
+              </div>
+            </div>
+            <div className="bullet-point">
+              <div className="bullet">2</div>
+              <div className="bullet-point-text">
+                <h4>Educate</h4>
+                <p>We'll educate them about assault and how to support you.</p>
+              </div>
+            </div>
+            <div className="bullet-point">
+              <div className="bullet">3</div>
+              <div className="bullet-point-text">
+                <h4>Empower</h4>
+                <p>Have a conversation of Empowerment instead of fear.</p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
